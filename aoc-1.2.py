93397cd00f7691ca8de188aed8ef804a5ecc6eb4
Alex Kompos <alex.kompos@rea-group.com>
2019-12-02 23:38:40 +1100

    day 1.2

---
+++ aoc-1.2.py
@@ -0,0 +1,17 @@
+running_sum = 0
+
+with open('inputs/aoc-1.txt', 'r') as file:
+    for module in file:
+        mass = int(module)
+
+        fuel = mass // 3 - 2
+        running_sum += fuel
+
+        while ((fuel // 3 - 2) > 0):
+            fuel = fuel // 3 - 2
+            running_sum += fuel
+
+
+
+print(running_sum)
+
