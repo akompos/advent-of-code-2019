5816017fdc031a54f7eb6330084a470bf8046982
Alex Kompos <alexkompos@gmail.com>
2019-12-08 22:48:53 +1100

    can you productionize AOC puzzles?

--- aoc-1.2.py
+++ aoc-1.2.py
@@ -1,17 +1,48 @@
-running_sum = 0
+import pytest
+from typing import List
 
-with open('inputs/aoc-1.txt', 'r') as file:
-    for module in file:
+
+
+
+
+
+def compute(s: str) -> int:
+    running_sum = 0
+    for module in s.splitlines():
         mass = int(module)
 
         fuel = mass // 3 - 2
         running_sum += fuel
 
-        while ((fuel // 3 - 2) > 0):
+        while (fuel // 3 - 2) > 0:
             fuel = fuel // 3 - 2
             running_sum += fuel
+    return running_sum
 
 
+def test(input_l: List[int], expected: int) -> None:
+    assert compute(input_l, ) == expected
 
-print(running_sum)
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('14', 2),
+        ('1969', 966),
+        ('100756', 50346),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
 
+
+def main() -> int:
+
+    with open('inputs/aoc-1.txt', 'r') as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+
+    exit(main())
--- aoc-1.py
+++ aoc-1.py
@@ -1,12 +1,40 @@
-running_sum = 0
+import pytest
+from typing import List
 
-with  open('inputs/aoc-1.txt', 'r') as file:
-    for module in file:
+
+def compute(s: str) -> int:
+    running_sum = 0
+    for module in s.splitlines():
         mass = int(module)
         fuel = mass // 3 - 2
         running_sum += fuel
+    return running_sum
 
 
+def test(input_l: List[int], expected: int) -> None:
+    assert compute(input_l, ) == expected
 
-print(running_sum)
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('12', 2),
+        ('14', 2),
+        ('1969', 654),
+        ('100756', 33583),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
 
+
+def main() -> int:
+
+    with open('inputs/aoc-1.txt', 'r') as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+
+    exit(main())
